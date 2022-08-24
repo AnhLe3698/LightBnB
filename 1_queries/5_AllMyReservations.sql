575e4adbfe7b16a15430b22118fba59af597c8c3
AnhLe3698 <anhviet.le00@hotmail.com>
2022-08-23 18:07:34 -0600

    Add Web App files

--- 1_queries/5_AllMyReservations.sql
+++ 1_queries/5_AllMyReservations.sql
@@ -11,7 +11,7 @@ SELECT reservations.id, properties.title, properties.cost_per_night,
 reservations.start_date, AVG(property_reviews.rating) AS average_rating
 FROM reservations
 JOIN properties ON properties.id = reservations.property_id
-JOIN property_reviews ON reservations.id = property_reviews.reservation_id
+JOIN property_reviews ON properties.id = property_reviews.property_id
 WHERE reservations.guest_id = 1
 GROUP BY reservations.id, properties.title, reservations.start_date,
 properties.cost_per_night
